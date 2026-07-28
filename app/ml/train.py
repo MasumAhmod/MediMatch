288db976022fa20089356e0a7dbb198bbd098ce7
Masum <masumahmod332@gmail.com>
2026-07-29 00:29:16 +0600

    Added ML files

--- app/ml/train.py
+++ app/ml/train.py
@@ -3,8 +3,8 @@ from sklearn.model_selection import train_test_split
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.linear_model import LogisticRegression
-from sklearn.metrics import accuracy_score
 from sklearn.metrics import classification_report
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
 import argparse
 from pathlib import Path
 import pandas as pd
@@ -48,11 +48,35 @@ lr.fit(X_train, y_train)
 pred = dt.predict(X_test)
 # print(f"Decision Tree Accuracy: {accuracy_score(y_test, pred)}")
 
-# pred = rf.predict(X_test)
+pred = rf.predict(X_test)
 # print(f"Random Forest Accuracy: {accuracy_score(y_test, pred)}")
 
-# pred = lr.predict(X_test)
+pred = lr.predict(X_test)
 # print(f"Logistic Regression Accuracy: {accuracy_score(y_test, pred)}")
 
 #report
-print(classification_report(y_test, pred))
+# print(classification_report(y_test, pred))
+
+models = {
+    "Decision Tree": dt,
+    "Random Forest": rf,
+    "Logistic Regression": lr
+}
+
+results = []
+
+for name, model in models.items():
+
+    pred = model.predict(X_test)
+
+    results.append({
+        "Model": name,
+        "Accuracy": accuracy_score(y_test, pred),
+        "Precision": precision_score(y_test, pred, average="weighted"),
+        "Recall": recall_score(y_test, pred, average="weighted"),
+        "F1-score": f1_score(y_test, pred, average="weighted")
+    })
+
+results_df = pd.DataFrame(results)
+
+print(results_df)
